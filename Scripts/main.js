a277eb3192b0046ae3c6e37fdf48ddac6370aad0
JesseVolstruis <2597626@students.wits.ac.za>
2025-04-30 18:37:28 +0200

    Totop button appears when scrolled

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,6 +2,7 @@
 const links = document.querySelectorAll('a');
 const staticOverlay = document.getElementById('StaticOverlay');
 
+//Activates static when changing pages or clicking to top buttons
 links.forEach(link => {
     link.addEventListener('click', function(e) {
         if (this.target === "_blank") {
@@ -28,7 +29,29 @@ links.forEach(link => {
     });
   });
 
+  //Removes static when page is shown
   window.addEventListener('pageshow', function(event) {
     staticOverlay.style.display = 'none';
   });
 
+  window.onscroll = function() {scrollFunction()};
+
+  //FOR PERFORMANCE might be constantly polling totop, could cause issues
+  function scrollFunction()
+  {
+    const toTop = document.querySelector(".ToTop");
+    if(toTop){
+    const toTopNav = toTop.closest('nav');
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+      toTop.style.bottom = "25px";
+      //toTopNav.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+     
+      
+    } else {
+      toTop.style.bottom = "-50px";
+      //toTopNav.style.backgroundColor = "rgba(0, 0, 0, 0)";
+    }
+  }
+}
+
+  
